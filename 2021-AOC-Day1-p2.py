f030c2e39fb6a2b97fc60579cdc81c74ee4ea7e3
epi55 <87436238+epi55@users.noreply.github.com>
2023-09-21 20:08:55 -0700

    Update 2021-AOC-Day1-p2.py

--- 2021-AOC-Day1-p2.py
+++ 2021-AOC-Day1-p2.py
@@ -1,11 +1,12 @@
 # Advent of Code
-# 2021 - Day 1 - Part 1
+# 2021 - Day 1 - Part 2
 # Source: https://adventofcode.com/2021/day/1
 
 ## IMPORTS ##
-with open(r'C:\Users\malaf\OneDrive\Desktop\Workspace\Code\Learning\2021 AOC\2021-AOC-Day-1-Puzzle-input.txt') as file:
+my_file = (r'2021 AOC\2021-AOC-Day-1-Puzzle-input.txt')
+with open(my_file, "r") as file:
     data = [int(line.strip()) for line in file]
-
+    
 ## SETUP
 depthInc = 0
 sumData = []
