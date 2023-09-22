9e2f5e4cfd6e98be4b4864d752bfe2b4a8d8a88d
epi55 <87436238+epi55@users.noreply.github.com>
2023-09-21 17:06:52 -0700

    Add files via upload

---
+++ 2021-AOC-Day-2-p1.py
@@ -0,0 +1,58 @@
+# Advent of Code
+# 2021 - Day 2 - Part 1
+# Source: https://adventofcode.com/2021/day/2
+
+## IMPORTS ##
+import re
+with open(r'C:\Users\malaf\OneDrive\Desktop\Workspace\Code\Learning\2021 AOC\2021-AOC-Day-2-Puzzle-input.txt') as file:
+    data = [line.strip() for line in file]
+
+## ENGINES ##
+instructions = []
+def instructionEngine(list):
+
+    for i in range(len(list)):
+        value = re.search(r'\d+', list[i])
+        if 'forward' in list[i]:
+            instructions.append(['forward', int(value.group())])
+        if 'down' in list[i]:
+            instructions.append(['down', int(value.group())])
+        if 'up' in list[i]:
+            instructions.append(['up', int(value.group())])
+    
+moveHorizontal = 0
+moveDepth = 0
+def movementEngine(list):
+    global moveHorizontal
+    global moveDepth
+    for i in range(len(list)):
+        if list[i][0] == "forward":
+            moveHorizontal += list[i][1]
+        if list[i][0] == "down":
+            moveDepth += list[i][1]
+        if list[i][0] == "up":
+            moveDepth -= list[i][1]
+
+def valueEngine(hor, dep):
+
+    print(hor * dep) # Correct answer: 1868935
+
+## RUN ##
+instructionEngine(data)
+movementEngine(instructions)
+valueEngine(moveHorizontal, moveDepth)
+
+## PSEUDO ##
+'''
+> Take input and create a list (data) where each command is its own element
+> Declare two types of variables: depth and horizontal
+> Create a function to interpret each instruction (list index)
+    > Create a nested list
+        > The command is the first index of a nested list
+        > The amount is the second index of a nested list
+> Create a function to interpret the nested list
+    > Forward adds to the Horizontal variable
+    > Down adds to the Depth variable
+    > Up subtracts from the Depth variable
+> Return the multiplication of Horizonal and Depth variables.
+'''
