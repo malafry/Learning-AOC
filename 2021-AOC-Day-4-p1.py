7ae8b5489c567f72e8aaf978c40230cb43f71ab9
epi55 <87436238+epi55@users.noreply.github.com>
2023-10-06 10:07:06 -0700

    Update 2021-AOC-Day-4-p1.py

--- 2021-AOC-Day-4-p1.py
+++ 2021-AOC-Day-4-p1.py
@@ -11,15 +11,9 @@ instructions = data[0].split(',')
 
 dataCopy = [item for item in data[1:] if item]
 
-# Code is functional until here
-# Instructions are parsed and dataCopy is only board rows
-# All boards will be 5x5, which means each index in dataCopy is a board row
-# Can divide length of dataset by 5 to determine the number of boards in dataCopy
-# Cannot create unique variables, so everything should be a separate nest within a board list
-
 ## ENGINES ##
 def createBoardsEngine(dataCopy, boardSize):
-    
+
     boardList = []
     for i in range(int(len(dataCopy) / boardSize)):
         if i == 0:
@@ -27,27 +21,57 @@ def createBoardsEngine(dataCopy, boardSize):
         boardList.append(dataCopy[(count):(count + boardSize)])
         count += boardSize
 
-    print("boardList at step 1: {}".format(boardList)) # TEST
-
     for i in range(len(boardList)):
         for j in range(len(boardList[0])):
             boardList[i][j] = list(map(int, boardList[i][j].split()))
 
-    print("boardList at step 2: {}".format(boardList)) # TEST
-
     boardKeys = []
     for i in range(len(boardList)):
         boardKeys.append("B{}".format(i + 1))
-    
+  
+    global boardDictionary
     boardDictionary = dict(zip(boardKeys, boardList))
     
+    global resultsLog
+    resultsLog = dict.fromkeys(boardKeys, [])
+
     print("boardDictionary[B1] at step 3: {}".format(boardDictionary['B1'])) # TEST
     print("boardDictionary[B2] at step 3: {}".format(boardDictionary['B2'])) # TEST
     print("boardDictionary[B3] at step 3: {}".format(boardDictionary['B3'])) # TEST
 
+def instructionEngine(instructions):
+    for i in range(len(instructions)): # 27 instructions
+        for b in range(len(boardDictionary)): # 3 boards
+            for row in range(len(boardDictionary['B{}'.format(b + 1)])): #5 rows
+                for col in range(len(boardDictionary['B{}'.format(b + 1)][row])): #5 cols
+                    if int(instructions[i]) == boardDictionary['B{}'.format(b + 1)][row][col]:
+                        #boardDictionary['B{}'.format(b + 1)][row][col] = '*'
+                        resultsLog['B{}'.format(b + 1)].append("R{} C{}".format(row, col))
+                        # Both lines above likely work, but I need to determine whether a Bingo is made each iteration
+                        # Try to use the checkEngine() as a test
+                        checkEngine()
+
+def checkEngine():
+    pass
+    # Start here.
+    # Consider a regex looking for five row Ns or five col Ns (if using the resultsLog)
+    # Otherwise, consider a regex looking for an asterix ... might not be as easy as above.
+    # The trick will be doing the calculation.
+
+def runEngine(boardSize):
+    createBoardsEngine(dataCopy, boardSize)
+    instructionEngine(instructions)
+
+    # TESTS
+    print("resultsLog[B1]: {}".format(resultsLog['B1'])) # TEST
+    print("resultsLog[B2]: {}".format(resultsLog['B2'])) # TEST
+    print("resultsLog[B3]: {}".format(resultsLog['B3'])) # TEST 
+    print("boardDictionary[B1]: {}".format(boardDictionary['B1'])) # TEST
+    print("boardDictionary[B2]: {}".format(boardDictionary['B2'])) # TEST
+    print("boardDictionary[B3: {}".format(boardDictionary['B3'])) # TEST
 
 ## RUN ##
-createBoardsEngine(dataCopy, boardSize = 5)
+runEngine(boardSize = 5)
 
 ## PSEUDOCODE ##
 '''
