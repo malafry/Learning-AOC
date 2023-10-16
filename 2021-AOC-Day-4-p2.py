d0ca561c0ef845365b229f4d5b8109b0358f694a
epi55 <87436238+epi55@users.noreply.github.com>
2023-10-15 22:39:34 -0700

    Update 2021-AOC-Day-4-p2.py

--- 2021-AOC-Day-4-p2.py
+++ 2021-AOC-Day-4-p2.py
@@ -45,7 +45,7 @@ def instructionEngine(instructions):
     for i in range(len(instructions)): # 27 instructions
         for b in range(len(boardDictionary)): # 3 boards
             if (('B{}').format(b + 1) in winningBoards):
-                print("Break @ i ({}) with board B{}".format(i, (b + 1)))
+                #print("Continue statement @ i ({}) with board B{}".format(i, (b + 1))) # TEST
                 continue
             for row in range(len(boardDictionary['B{}'.format(b + 1)])): #5 rows
                 for col in range(len(boardDictionary['B{}'.format(b + 1)][row])): #5 cols
@@ -54,10 +54,10 @@ def instructionEngine(instructions):
                         resultsLog[b].append("R{}".format(row + 1))
                         resultsLog[b].append("C{}".format(col + 1))
                         if (checkEngine(resultsLog, b, i) == True):
-                            print("Test: {}".format(winningBoards)) # TEST
                             key = "B{}".format(b + 1)
                             winningPoints = int(instructions[i])* (int(sum([num for sublist in boardDictionary[key] for num in sublist if isinstance(num, int)])))
                             return(print("Last winning board: {}. Last value: {}. Winning points: {}. i: {}".format((b + 1), instructions[i], winningPoints, i)))                        
+                            # LAST WINNING BOARD should be: i: 14 = 13
 
 def checkEngine(resultsLog, board, iter):
     count = 0
@@ -65,15 +65,13 @@ def checkEngine(resultsLog, board, iter):
     for j in range(len(pattern)):
         logTemp = ', '.join(resultsLog[board])
         count = len(re.findall(pattern[j], logTemp))
-        if iter == 15: # iter 14 = 13 instruction # TEST
-            print(len(winningBoards)) # TEST
-            print(len(boardDictionary)) # TEST
         if count == 5:
             winningBoards.append("B{}".format(board + 1))
             print("Winning boards: {} @ i: {} = {}".format(winningBoards, iter, instructions[iter])) # TEST
             if iter == 14: # iter 14 = 13 instruction # TEST
-                print(len(winningBoards)) # TEST
-                print(len(boardDictionary)) # TEST
+                print("winningBoards @ i 14: {}".format(len(winningBoards))) # TEST
+                print("boardDictionary @ i 14: {}".format(len(boardDictionary))) # TEST
+                # ITER 14 is not being printed, so count == 5 @ iter 5.
             if (len(winningBoards) == len(boardDictionary)):
                 return True
         else:
