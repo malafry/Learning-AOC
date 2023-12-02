99704cdcad8d6e74269835513fa3703758452335
Robin <87436238+epi55@users.noreply.github.com>
2023-12-01 23:45:22 -0800

    Add files via upload

--- 2023-AOC-Day-1-p2.py
+++ 2023-AOC-Day-1-p2.py
@@ -4,7 +4,6 @@
 
 ## Imports
 import pathlib
-import re
 
 ## Setup and assignments
 input = pathlib.Path('Learning') / '2023 AOC' / '2023-AOC-Day-1-Puzzle-input.txt'
@@ -33,63 +32,29 @@ def frontNum(data, i):
                 break
         if frontValue != '':
             break
-    print("frontValue for {}: {}".format(i, frontValue))
     return frontValue
 
 def backNum(data, i):
     word = ''
     backValue = ''
-    if len(data[i]) == 1:
-        backValue = data[i]
-    else:
-        for l in range(len(data[i])-1,0,-1):
-            word = data[i][l] + word # Handles reverse
-            for w in range(len(lookUp)):
-                if lookUp[w][1] in word:
-                    backValue = lookUp[w][1]
-                    break
-                elif lookUp[w][0] in word:
-                    backValue = lookUp[w][1]
-                    break
-            if backValue != '':
+    for l in range(len(data[i]) - 1,-1,-1):
+        word = data[i][l] + word
+        for w in range(len(lookUp)):
+            if lookUp[w][1] in word:
+                backValue = lookUp[w][1]
                 break
-    print("backValue for {}: {}".format(i, backValue))
+            elif lookUp[w][0] in word:
+                backValue = lookUp[w][1]
+                break
+        if backValue != '':
+            break
     return backValue
 
 sum = 0
 
 for i in range(len(data)):
     dataEnds = frontNum(data, i) + backNum(data, i)
-    print(dataEnds)
+    print("{}: {}".format(i+1, dataEnds))
     sum += int(dataEnds)
 
-print(sum) # ANSWER WRONG: 54607 (too high) 53021 (too low)
-
-# TEST LOG
-#for i in range(len(data)):
-#    print("{} => {} => {}:{}".format(data2[i], data[i], data[i][0], data[i][-1]))
-
-## KEEP
-'''for i in range(len(data)):
-    word = ''
-    for l in range(len(data[i])):
-        word += data[i][l]
-        for w in range(len(lookUp)):
-            if lookUp[w][0] in word:
-                word = word.replace(lookUp[w][0], lookUp[w][1])
-    data[i] = word
-    data[i] = re.findall(r"\d+", data[i])
-    data[i] = ''.join(data[i])
-    dataEnds = int(str(data[i][0]) + str(data[i][-1]))
-    sum += dataEnds'''
-
-## KEEP
-'''    for l in range(len(data[i]),0,-1):
-        word += data[i][l]
-        print(word)
-        wordRev = word[::-1]
-        print(wordRev)
-        for w in range(len(lookUp)):
-            if lookUp[w][0] in wordRev:
-                wordRev = wordRev.replace(lookUp[w][0], lookUp[w][1])
-                print("REV!: {}".format(wordRev))'''
+print(sum)
