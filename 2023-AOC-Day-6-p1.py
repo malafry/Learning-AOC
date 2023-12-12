b20ab719e55829ab4cf9e5302dff0a89079d914a
Robin <87436238+epi55@users.noreply.github.com>
2023-12-11 22:15:20 -0800

    Add files via upload

--- 2023-AOC-Day-6-p1.py
+++ 2023-AOC-Day-6-p1.py
@@ -12,10 +12,36 @@ with open(input, 'r') as file:
 
 time_str = input[0].split(':')[1].strip().split()
 time_int = [int(x) for x in time_str]
-distance_str = input[1].split(':')[1].strip().split()
-distance_int = [int(x) for x in distance_str]
+record_distance_str = input[1].split(':')[1].strip().split()
+record_distance_int = [int(x) for x in record_distance_str]
 
-raceData = {'time': time_int, 'distance': distance_int}
+raceData = {'time': time_int, 'record_distance': record_distance_int}
 
-print(raceData)
+def distance(i, time, record_distance):
+    wins = list()
+    for time_i in range(time):
+        time_held = time_i # aka speed
+        for time_in_motion in range(time - time_held + 1):
+            distance = time_held * time_in_motion
+            if distance > record_distance:
+                if time_held not in wins:
+                    wins.append(time_held)
+    if wins:
+        scores.append(len(wins))    
+
 ## ENGINE
+scores = list()
+winningTotal = 1
+
+for i in range(len(raceData['time'])):
+    distance(i, raceData['time'][i], raceData['record_distance'][i])
+
+print("Score: {}. Winning total: {}.".format(scores, scores[0] * scores[1] * scores[2] * scores[3]))
+
+'''
+time = x
+record_distance = y
+speed = time_held
+time_moving = time_held
+possibilities = time
+'''
