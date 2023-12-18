d5ef479d6dea3b50875e5169f078ee97a040067e
Robin <87436238+epi55@users.noreply.github.com>
2023-12-17 20:29:58 -0500

    Add files via upload

--- 2023-AOC-Day-8-p2.py
+++ 2023-AOC-Day-8-p2.py
@@ -10,63 +10,56 @@ input = pathlib.Path('Learning') / '2023 AOC' / '2023-AOC-Day-8-Puzzle-input.txt
 with open(input, 'r') as file:
     input = [line.strip() for line in file]
 
-def setup(input, start, directions, networkStartList):
-    directions = list(input[0])
-    network = list()
+def setup(input, begin_char, network_map, ghosts):
     for node in input[2:]:
         key = node.split('=')[0].strip()
-        valueLeft = node.split('=')[1].split(',')[0].strip(' (')
-        valueRight = node.split('=')[1].split(',')[1].strip(' )')
-        networkDict[key] = [valueLeft, valueRight]
-        tempDict = {key: [valueLeft, valueRight]}
-        network.append(tempDict)
+        value_left = node.split('=')[1].split(',')[0].strip(' (')
+        value_right = node.split('=')[1].split(',')[1].strip(' )')
+        network_node = {key: [value_left, value_right]}
+        network_map.append(network_node)
+    for node in network_map:
+        node_key = list(node.keys())[0]
+        if begin_char in node_key:
+            ghosts.append({'start': node_key, 'current': node_key, 'status': False})
 
-    # INIT START LIST - network not used
-    for node in network:
-        nodeKey = list(node.keys())[0]
-        if start in nodeKey:
-            networkStartList.append(nodeKey)
-
-def run(directions, networkStartList, step, totalSteps):  
-    for key in networkStartList:
-        location = key
-        repeat = True
-        print('1')
-        print(directions[:])
-        while repeat:
-            for dir in directions[:]:
-                print(dir)
-                if dir == 'L':
-                    print('L')
-                    location = networkDict[location][0]
-                    print(location) # TEST
-                    step += 1
-                if dir == 'R':
-                    print('R')
-                    location = networkDict[location][1]
-                    print(location) # TEST
-                    step += 1
-                if 'Z' in location:
-                    print('Z')
-                    networkStartList.remove(key)
-                    totalSteps.append(step)
-                    step = int()
-                    repeat = False
+def run(instructions, network_map, ghosts, step):  
+    continue_loop = True
+    for dir in instructions:
+        for ghost in ghosts:
+            if dir == 'L':
+                # location = network_map[location][0]
+                ghost['current'] = updateLoc(0, ghost['current'], network_map)
+                step += 1
+            if dir == 'R':
+                # location = network_map[location][1]
+                ghost['current'] = updateLoc(1, ghost['current'], network_map)
+                step += 1
+            if 'Z' in ghost['current']:
+                ghost['status'] = True
+            
+        checkLoc(ghosts, step, continue_loop)
     
-    if networkStartList:
-        print('2')
-        run(directions, networkStartList, step, totalSteps)
-    else:
-        print('3')
-        print(totalSteps)
+    if continue_loop:
+        run(instructions, network_map, ghosts, step)
+
+def updateLoc(dir, location, network_map):
+    for network_node in network_map:
+        if location in network_node.keys():
+            return network_node[location][dir]
+
+def checkLoc(ghosts, step, continue_loop):
+    for ghost in ghosts:
+        if ghost['status'] == False:
+            break
+        else:
+            print("Total steps: {}".format(step))
+            continue_loop = False
 
 ## ENGINE
-start = 'A' # A sample, Z real
-directions = list()
-networkStartList = list()
+begin_char = 'A' # A sample, Z real
+instructions = list(input[0])
+network_map = list()
+ghosts = list()
 step = int()
-totalSteps = list()
-networkDict = dict()
-setup(input, start, directions, networkStartList)
-run(directions, networkStartList, step, totalSteps)
-print(networkStartList)
+setup(input, begin_char, network_map, ghosts)
+run(instructions, network_map, ghosts, step)
